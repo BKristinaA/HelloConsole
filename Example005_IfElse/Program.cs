82a8930d63d98262966510cc7b14882d78c1454c
Christina <bihtya@mail.ru>
2023-01-17 19:36:17 +0300

    Привет, Маша

--- Example005_IfElse/Program.cs
+++ Example005_IfElse/Program.cs
@@ -1,15 +1,15 @@
-﻿Console.Write("Введите имя:");
+﻿Console.Write("Введите имя: ");
 string username = Console.ReadLine();
 
 if(username == "Маша")
 {
-    Console.WriteLine("Ура, это же МАША");
+    Console.WriteLine("Ура, это же Маша");
 }
 
 else
 
 {
-    Console.Write("Привет,");
-    Console.WriteLine();
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
 }
 
